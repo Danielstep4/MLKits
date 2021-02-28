bda159796f00a9261464e5c62f784c73b9caa736
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-02-28 20:34:05 +0200

    Working through tutorial

--- plinko/score.js
+++ plinko/score.js
@@ -5,16 +5,20 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
 function runAnalysis() {
 	// K-Nearest Neighbor
-	const analyzeOutputs = []
-	const k = 3
+	const k = outputs.length / 3
 	const predicationPoint = 300
 	function distance(point) {
 		return Math.abs(point - predicationPoint)
 	}
-	for(let dp of outputs) {
-		let x = distance(dp[0])
-		analyzeOutputs.push([x ,dp[3]])
-	}
-	console.log(analyzeOutputs.sort((a, b) => a[0] - b[0]).slice(0,k))
+	const reuslt = _.chain(outputs)
+		.map(row => [distance(row[0]), row[3]])
+		.sortBy(row => row[0])
+		.slice(0, k)
+		.countBy(row => row[1])
+		.toPairs()
+		.sortBy(row => row[1])
+		.last()
+		.first()
+		.parseInt()
+		.value()
 }
-
