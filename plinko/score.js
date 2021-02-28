cd51b0bb642134c5261dba5cdebaacc17604e1dc
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-02-28 19:54:13 +0200

    ok

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,20 @@
+const outputs = []
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  	outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+	// K-Nearest Neighbor
+	const analyzeOutputs = []
+	const k = 3
+	const predicationPoint = 300
+	function distance(point) {
+		return Math.abs(point - predicationPoint)
+	}
+	for(let dp of outputs) {
+		let x = distance(dp[0])
+		analyzeOutputs.push([x ,dp[3]])
+	}
+	console.log(analyzeOutputs.sort((a, b) => a[0] - b[0]).slice(0,k))
 }
 
